6ac975c4d8e74f877e7b4c53c0d2c416303b4706
Hugo Delgado <hugo.delgado1@outlook.com>
2022-06-08 17:43:55 -0500

    fetch request has begun however having cors header issues

--- client/src/components/reviews/Review.js
+++ client/src/components/reviews/Review.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect  } from 'react';
+import axios from 'axios';
 import ReviewCard from './ReviewCard.js';
+import Box from '@mui/material/Box';
 
 function Review(){
-  return (
-    <div>
-      <h1> Reviews </h1>
-      <ReviewCard />
-    </div>
-  );
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [reviews, setReviews] = useState([]);
+
+
+  useEffect(() => {
+    fetch('http://localhost:5000/reviews')
+    .then(res => res.json())
+    .then(
+      (result) => {
+        console.log(`These are the results ${result}`);
+        setIsLoaded(true);
+        setReviews(result);
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+  }, [])
+
+  if(error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div>
+        <h1> Reviews </h1>
+        {reviews.map( (review, index) => {
+          <li key={index}>
+            {review.name}
+          </li>
+        })}
+        <ReviewCard />
+      </div>
+    );
+  }
 }
 export default Review;
