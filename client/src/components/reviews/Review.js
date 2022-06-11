db34c752f86e2157612c7f0df91faf7dade43038
Hugo Delgado <hugo.delgado1@outlook.com>
2022-06-10 22:38:15 -0500

    working with styling for review card

--- client/src/components/reviews/Review.js
+++ client/src/components/reviews/Review.js
@@ -1,16 +1,35 @@
-import React, { useState} from 'react';
+import React, { useState, useRef, useReducer } from 'react';
 import { useSelector } from 'react-redux';
 import ReviewCard from './ReviewCard.js';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
 import ArrowForwardIosOutlinedIcon from '@mui/icons-material/ArrowForwardIosOutlined';
+import Slide from '@mui/material/Slide';
 
+const initialState = { count: 0 };
 
+function reducer(state, action) {
+  switch(action.type){
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+}
+
+const handleChange = () => {
+
+}
+
+let reveal = 'hidden';
 
 function Review(){
     const reviews = useSelector((state) => state.reviews)
     const [current, setCurrent] = useState(0);
+    const [state, dispatch] = useReducer(reducer, initialState)
     const length = reviews.length;
 
   return (
@@ -18,9 +37,9 @@ function Review(){
       <Box sx={{ width: "100%", height: '20%' }}>
       <Typography align="center" variant="h2"> Reviews </Typography>
       <Box sx={{ display: 'flex', flexDirection: 'row', position: 'relative', justifyContent: 'center' }}>
-      <ArrowBackIosNewOutlinedIcon className='left-arrow' />
+      <ArrowBackIosNewOutlinedIcon className='left-arrow' onClick={handleChange} />
       {reviews.map((review, index) => {
-           return <ReviewCard className='review'  review={review} key={index} />
+           return <ReviewCard style={ index !== state ? {display:'none'} : ''  }  review={review} key={index} />
       })}
       <ArrowForwardIosOutlinedIcon className='right-arrow' />
       </Box>
